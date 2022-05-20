36ea691bb2e3930e71037b60cc2246de7fc61f5c
OmgCopito95 <july1995bolt@gmail.com>
2022-05-19 22:38:51 -0300

    ej 1 - servidor basico http

---
+++ ejemplo_1/index.js
@@ -0,0 +1,11 @@
+// CREA UN SERVIDOR HTTP Y UN CLIENTE HTTP
+
+var http = require('http'); // importa el modulo http
+
+http.createServer(function(req, res) { // crea un servidor http
+    res.writeHead(200, {'Content-Type': 'text/html'}); // establece el tipo de contenido
+    res.end('<h1>Hola mundo</h1>'); // escribe el contenido
+}).listen(5000); // escucha en el puerto 8080
+
+console.log('Servidor ejemplo_1 corriendo en http://localhost:5000'); // muestra un mensaje en la consola
+
