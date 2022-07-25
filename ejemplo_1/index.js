b7796429175bd4635aabf11b7def72cc29813580
Copito <july1995bolt@gmail.com>
2022-07-25 17:49:36 -0300

    correccion de comentario

--- ejemplo_1/index.js
+++ ejemplo_1/index.js
@@ -5,7 +5,7 @@ var http = require('http'); // importa el modulo http
 http.createServer(function(req, res) { // crea un servidor http
     res.writeHead(200, {'Content-Type': 'text/html'}); // establece el tipo de contenido
     res.end('<h1>Hola mundo</h1>'); // escribe el contenido
-}).listen(5000); // escucha en el puerto 8080
+}).listen(5000); // escucha en el puerto 5000
 
 console.log('Servidor ejemplo_1 corriendo en http://localhost:5000'); // muestra un mensaje en la consola
 
